728170d1777927cf817141396ff97513437d993c
Arunkumar Rathinavel <arunrails20@gmail.com>
2019-09-18 12:06:04 +0530

    const examples

--- fundamental/main.go
+++ fundamental/main.go
@@ -4,17 +4,31 @@ import "fmt"
 
 func main() {
 
-    // string
-    var a = "this is string"
-    fmt.Println(a)
-    // int
-    var i = 20
-    fmt.Println(i)
-    // boolean 
-    var s bool = true
-    fmt.Println(s)
-    // default value as 0
-    var l int
-    fmt.Println(l)
+ // string
+ var a = "this is string"
+ fmt.Println(a)
+ // int
+ var i = 20
+ fmt.Println(i)
+ // boolean 
+ var s bool = true
+ fmt.Println(s)
+ // default value as 0
+ var l int
+ fmt.Println(l)
+ //calling function
+ constEx()
+ // if you call only constEx,
+ // will error msg "constEx evaluated but not used"
+ //constEx
 
+}
+
+func constEx() {
+  fmt.Println(s)
+  const b string = "java"
+  fmt.Println(b)
+  // will raise error "cannot assign to b"
+  //b = "py"
+  //fmt.Println(b)
 }
