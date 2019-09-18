b9438d8e5baf8cc55149d51f8790d73dc1084470
Arunkumar Rathinavel <arunrails20@gmail.com>
2019-09-18 11:12:28 +0530

    variables

---
+++ fundamental/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    // string
+    var a = "this is string"
+    fmt.Println(a)
+    // int
+    var i = 20
+    fmt.Println(i)
+    // boolean 
+    var s bool = true
+    fmt.Println(s)
+    // default value as 0
+    var l int
+    fmt.Println(l)
+
+}
